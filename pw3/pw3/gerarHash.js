1568dd9eef8271a80587453db0bd14704816f280
kaua-oli80 <kauar7576@gmail.com>
2026-03-30 08:59:47 -0300

    Delete pw3/pw3 directory

--- pw3/pw3/gerarHash.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt';
-
-const senhaUsuario = "bruno@access#99"; // Coloque aqui a senha que você quer transformar
-const saltRounds = 10;
-
-bcrypt.hash(senhaUsuario, saltRounds, (err, hash) => {
-    console.log(`Senha original: ${senhaUsuario}`);
-    console.log(`Hash gerado: ${hash}`);
-});
